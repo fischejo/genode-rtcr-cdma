384bb3ad3cc93d693b622803dd1f1b0a586130e0
johannes.m.fischer <ga97tay@mytum.de>
2019-09-23 20:48:39 +0200

    fix errors

--- src/rtcr_cdma/pd_session.cc
+++ src/rtcr_cdma/pd_session.cc
@@ -4,7 +4,7 @@
  * \date   2019-08-29
  */
 
-#include <rtcr_cdma/ram_cdma_session.h>
+#include <rtcr_cdma/pd_session.h>
 
 using namespace Rtcr;
 
@@ -22,11 +22,12 @@ using namespace Rtcr;
 #endif
 
 Pd_cdma_session::Pd_cdma_session(Genode::Env &env,
-				   Genode::Allocator &md_alloc,
-				   const char *creation_args,
-				   Child_info *child_info)
+				 Genode::Allocator &md_alloc,
+				 Genode::Entrypoint &ep,
+				 const char *creation_args,
+				 Child_info *child_info)
 	:
-	Pd_session(env, md_alloc, creation_args, child_info),
+	Pd_session(env, md_alloc, ep, creation_args, child_info),
 	_cdma_drv(env)
 {
 	DEBUG_THIS_CALL;
@@ -43,7 +44,7 @@ void Pd_cdma_session::_destroy_dataspace(Ram_dataspace *ds)
 	if(!ds->i_cached) {
 		Genode::destroy(_md_alloc, (Physical_address *)ds->storage);
 	}
-	Ram_session::_destroy_dataspace(ds);
+	Pd_session::_destroy_dataspace(ds);
 }
 
 
@@ -59,7 +60,7 @@ void Pd_cdma_session::_alloc_dataspace(Ram_dataspace *ds)
 void Pd_cdma_session::_attach_dataspace(Ram_dataspace *ds)
 {
 	DEBUG_THIS_CALL;
-	Ram_session::_attach_dataspace(ds);
+	Pd_session::_attach_dataspace(ds);
 
 	if(!ds->i_cached) {
 		/* also directly calculate the physical address. I assume that it will
